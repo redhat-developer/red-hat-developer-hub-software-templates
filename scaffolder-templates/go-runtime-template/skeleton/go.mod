60a898159c4e5f71d315c70eae4a1c84c33472d4
Paul Schultz <pschultz@pobox.com>
2023-03-03 13:12:38 -0600

    add scaffolder-skeletons
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

---
+++ scaffolder-templates/go-runtime-template/skeleton/go.mod
@@ -0,0 +1,3 @@
+module github.com/${{values.github_org | dump}}/${{values.repo_name | dump}}
+
+go 1.16
---
+++ scaffolder-templates/python-template/skeleton/src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="0.0.0.0")
