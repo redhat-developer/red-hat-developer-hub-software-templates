c9797064284f29cb0ee8c99a91a99089b00bb254
Patrick Knight <pknight@redhat.com>
2023-03-08 13:52:59 -0500

    Add Quarkus GPT

---
+++ scaffolder-templates/quarkus-gpt-template/skeleton/src/test/java/${{values.java_package_name}}/GreetingResourceTest.java
@@ -0,0 +1,21 @@
+package ${{values.group_id}}.${{values.artifact_id}};
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello-resteasy")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
