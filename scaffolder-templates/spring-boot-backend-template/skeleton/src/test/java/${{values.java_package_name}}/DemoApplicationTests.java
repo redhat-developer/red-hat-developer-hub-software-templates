521d5f3a3fa50fb5ff6198a9f4bdfb11e92aa8c6
Patrick Knight <82788581+PatAKnight@users.noreply.github.com>
2023-03-08 09:55:47 -0500

    Add Spring Boot template (#67)
    * Add Spring Boot template
    
    * Add git attributes

---
+++ scaffolder-templates/spring-boot-backend-template/skeleton/src/test/java/${{values.java_package_name}}/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ${{values.group_id}}.${{values.artifact_id}}
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
