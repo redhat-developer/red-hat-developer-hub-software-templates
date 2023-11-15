0d28d24eead295f2e315d594b24c8faa1f7d3a2e
Ioannis Canellos <iocanel@gmail.com>
2023-11-15 14:44:22 +0000

    chore: delete unused and out of date template (#195)

--- templates/github/quarkus-web/README.md
+++
@@ -1,7 +0,0 @@
-# Quarkus Web Template
-
-This repository contains the Backstage Template used to create the Kubernetes resources needed to build/deploy a simple quarkus application.
-
-## Repository Breakdown
-
-TBD
--- templates/github/quarkus-web/skeleton/src/test/java/${{values.java_package_name}}/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package ${{values.java_package_name}};
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
